db8f9a50fc7666bfc398e705672332f67cecd69d
user <radchenkobodya@gmail.com>
2015-05-15 17:02:21 +0300

    change in PM

--- test_order_nl.py
+++ test_order_nl.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -39,30 +40,38 @@ class test_order_nl(unittest.TestCase):
 
         #add to card
 
-        ran = random.randrange(2, 13)
-        for x in range(1, ran):
-            search =  self.driver.find_element_by_id("fc_search").send_keys(random.choice(product), Keys.ENTER)
-            add_tocard = self.driver.find_element_by_xpath("(//div[@id='selectedProduct']//div/button)[2]").click()
-            print("Add to card " + random.choice(product))
 
-        # steps to PM
-        click_card = self.driver.find_element(By.XPATH, "//div[@class='cartHeader']//a")
-        click_card.click()
-        step1 = self.driver.find_element(By.XPATH, "(//button[@class='btncta icon buy large checkout'])[1]")
-        step1.click()
-        step2 = self.driver.find_element(By.ID, "default")
-        step2.click()
-        go_pm = self.driver.find_element(By.XPATH, "(//form[@id='orderForm']//button)[1]")
-        go_pm.click()
 
         #pm
         for pmname in self.paymethod:
-            pm = self.driver.find_element(By.ID, '{0!s}'.format(pmname))
+
             if pmname == "ideal":
-               ideal = self.driver.find_elements(By.XPATH, "//select[@id='parentId-ideal']/option[position()>1]")
-               for i in ideal:
-                   print(i.get_attribute('value'))
-                #Select(self.driver.find_element(By.ID, "parentId-ideal")).select_by_value("INGBNL2A")
+                ran = random.randrange(2, 5)
+                for x in range(1, ran):
+                    search =  self.driver.find_element_by_id("fc_search").send_keys(random.choice(product), Keys.ENTER)
+                    add_tocard = self.driver.find_element_by_xpath("(//div[@id='selectedProduct']//div/button)[2]").click()
+                    print("Add to card " + random.choice(product))
+
+        # steps to PM
+                click_card = self.driver.find_element(By.XPATH, "//div[@class='cartHeader']//a")
+                click_card.click()
+                step1 = self.driver.find_element(By.XPATH, "(//button[@class='btncta icon buy large checkout'])[1]")
+                step1.click()
+                step2 = self.driver.find_element(By.ID, "default")
+                step2.click()
+                go_pm = self.driver.find_element(By.XPATH, "(//form[@id='orderForm']//button)[1]")
+                go_pm.click()
+                pm = self.driver.find_element(By.ID, '{0!s}'.format(pmname))
+                ideal = self.driver.find_elements(By.XPATH, "//select[@id='parentId-ideal']/option[position()>1]")
+                for i in ideal:
+                    try:
+                        Select(self.driver.find_element(By.ID, "parentId-ideal")).select_by_value('{0!s}'.format(i.get_attribute('value')))
+                        self.driver.find_element(By.XPATH, "(//button[@class='btncta icon buy large orderButton nextstep checkout'])[1]").click()
+
+                    except NoSuchElementException:
+                        self.driver.get("http://futurumshop.nl")
+
+
 
     def tearDown(self):
         self.driver.quit()
