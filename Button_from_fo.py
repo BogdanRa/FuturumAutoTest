9b2f953cb25b2738abf9ebd88b57a5688cf1e085
Bohdan <radchenkobodya@gmail.com>
2015-06-11 18:23:04 +0300

    xpath/id for FO button

--- Button_from_fo.py
+++ Button_from_fo.py
@@ -1,1 +1,11 @@
-__author__ = 'bohdan'
+
+search = "fc_search"
+add_to_card = "(//div[@id='selectedProduct']//div/button)[2]"
+go_card = "//div[@class='cartHeader']//a"
+two_step_in_card = "(//button[@class='btncta icon buy large checkout'])[1]"
+choose_default = "default"
+go_pm_page = "(//form[@id='orderForm']//button)[1]"
+buy_button = "(//button[@class='btncta icon buy large orderButton nextstep checkout'])[1]"
+
+
+
