5bef71aa56ceaf52cfadccc666667fb90cbc9382
Bohdan <radchenkobodya@gmail.com>
2015-06-11 18:22:18 +0300

    done 99%

--- order_nl.py
+++ order_nl.py
@@ -1,5 +1,5 @@
 from startwork import start_webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,8 +8,7 @@ import random
 #from  connect_to_bd import product_form_bd
 from selenium.webdriver.support.expected_conditions import alert_is_present
 from selenium.webdriver.support import expected_conditions as EC
-
-
+from  Button_from_fo import *
 
 class test_order_nl(unittest.TestCase):
 
@@ -18,16 +17,15 @@ class test_order_nl(unittest.TestCase):
         # prepare browsing and webdriver for work
         self.start = start_webdriver()
         self.driver = self.start.start_and_login()
-        self.xpath = self.start.find_by_xpath
-        self.id = self.start.find_by_id
+        self.xpath = self.start.click_by_xpath
+        self.click_by_id = self.start.click_by_id
         self.send_to_id = self.start.send_keys_id
         self.send_to_xpath = self.start.send_keys_xpath
 
         # done
 
-        self.paymethod = {"ideal":[ "ASNBNL21", "TRIONL2U", "FVLBNL22"], "paypal": 'paypal'}
-                          #"ogonestd":["visa", "aexpress", "mastercard"],"overboeking": 'overboeking', "rmbrs": 'rmbs', "mistercash": 'mistercash'}
-                           # "ABNANL2A",  "RABONL2U", "INGBNL2A", "KNABNL2H", "SNSBNL2A", "RBRBNL21",
+        self.paymethod = {"ideal":[ "FVLBNL22", "ABNANL2A",  "RABONL2U", "INGBNL2A", "KNABNL2H", "SNSBNL2A", "RBRBNL21", "ASNBNL21", "TRIONL2U"], "paypal": 'paypal', "ogonestd":["visa", "aexpress", "mastercard"],"overboeking": 'overboeking', "rmbrs": 'rmbs', "mistercash": 'mistercash'}
+
         self.ran = random.randrange(2, 10)
 
 
@@ -36,12 +34,7 @@ class test_order_nl(unittest.TestCase):
     def test_ordernl(self):
 
 
-        product = ["6003-0072-N1011", "6003-0136-N1202", "6004-0209-001-N1102",
-                   "6004-0209-002-N1102","6004-0293-001-N1108", "6004-0293-004-N1108", "6004-0294-003-N1108", "6004-0294-004-N1108",
-                   "6004-0296-N1108", "6004-0298-N1108", "6004-0300-N1108", "6004-0307-005-N1108", "6004-0385-N1208",
-                    "6004-0410-003-N1302", "6004-0410-004-N1302", "6004-0411-003-N1302", "6004-0431-001-N1302", "6004-0442-N1303",
-                   "6004-0459-N1304", "6004-0470-002-N1307", "6004-0470-004-N1307", "6004-0514-002-N1308", "6004-0519-004-N1308",
-                    "6004-0520-004-N1308", "6004-0577-001-N1502", "6004-0580-002-N1502", "6004-0591-003-N1502", "6004-0593-004-N1502"]
+        product = ["6003-0072-N1011"]
         # add products ( connect to mysql )
         #products = product_form_bd()
         #product = products.connect_to_mysql()
@@ -50,42 +43,49 @@ class test_order_nl(unittest.TestCase):
         for pmname in self.paymethod:
 
             for x in range(0, self.ran):
-                        #add card
-                        self.send_to_id("fc_search", (random.choice(product)))
-                        self.xpath("(//div[@id='selectedProduct']//div/button)[2]")
-                        print("Add to card " + random.choice(product))
+            #add card
+                try:
+                    self.send_to_id(search, (random.choice(product)))
+                    self.xpath(add_to_card)
+                except UnexpectedAlertPresentException:
+                    self.driver.switch_to_alert().accept()
 
-            #steps PM
-            self.xpath("//div[@class='cartHeader']//a")
-            self.xpath("(//button[@class='btncta icon buy large checkout'])[1]")
-            self.id("default")
-            self.xpath("(//form[@id='orderForm']//button)[1]")
+
+                print("Add to card " + random.choice(product))
+
+                # steps PM
+            self.xpath(go_card)
+            self.xpath(two_step_in_card)
+            self.click_by_id(choose_default)
+            self.xpath(go_pm_page)
+
+
 
 
 
 
             if pmname == "ideal":
                 for pmideal in self.paymethod[pmname]:
-                    #add card
-                    self.send_to_id("fc_search", (random.choice(product)))
-                    self.xpath("(//div[@id='selectedProduct']//div/button)[2]")
-                    print("Add to card " + random.choice(product))
+
+                    self.send_to_id(search, (random.choice(product)))
+                    self.xpath(add_to_card)
 
                     #steps PM
-                    self.xpath("//div[@class='cartHeader']//a")
-                    self.xpath("(//button[@class='btncta icon buy large checkout'])[1]")
-                    self.id("default")
-                    self.xpath("(//form[@id='orderForm']//button)[1]")
+                    self.xpath(go_card)
+                    self.xpath(two_step_in_card)
+                    self.click_by_id(choose_default)
+                    self.xpath(go_pm_page)
+
                     Select(self.driver.find_element(By.ID, "parentId-ideal")).select_by_value('{}'.format(pmideal))
-                    self.xpath("(//button[@class='btncta icon buy large orderButton nextstep checkout'])[1]")
+                    self.xpath(buy_button)
+                    self.driver.get("http://futurumshop.nl")
 
-                    #self.driver.get("http://futurumshop.nl")
-                    try:
-                        self.driver.get("http://futurumshop.nl")
 
-                    except NoSuchElementException, e:
-                        alarm = self.driver.switch_to_alert()
-                        alarm.accept()
+
+
+
+
+
 
 
 
@@ -95,7 +95,7 @@ class test_order_nl(unittest.TestCase):
 
 
                     Select(self.id("parentId-ideal")).select_by_value('{}'.format(pmogonestd))
-                    self.xpath("(//button[@class='btncta icon buy large orderButton nextstep checkout'])[1]")
+                    self.xpath(buy_button)
 
 
 
@@ -103,8 +103,8 @@ class test_order_nl(unittest.TestCase):
 
                     self.xpath("//input[@id='{}']".format(pmname))
 
-                    self.xpath("(//button[@class='btncta icon buy large orderButton nextstep checkout'])[1]")
-                    self.driver.get("http://futurumshop.nl")
+                    self.xpath(buy_button)
+
 
 
     def tearDown(self):
