de43e22b24d1be6dc9c02d7db8bbb8ef85aba926
Bohdan <radchenkobodya@gmail.com>
2015-12-07 12:32:10 +0200

    Open it from console

--- testimport/createimport.py
+++ testimport/createimport.py
@@ -1,4 +1,5 @@
 from random import *
+
 from faker import Faker
 '''
 Creating import file with 7 important columns (vendorID, productID, vendorName,  productName, adviesprijs, verkoopprijs, product stock) 
@@ -12,14 +13,14 @@ need some refactoring
 #make a product name
 Namegenerator = Faker()
 
-
+count = raw_input("How much products you need ?: ")
 #product stock
 productstock = ['2-6 werkdagen', 'ja, op voorraad', 'Onbekende levertijd', 'week 1',
 				'week 10', 'week 11', 'week 12', 'week 13']
 
 #creating import file with 7 columns (vendorID, productID, vendorName,  productName, adviesprijs, verkoopprijs, product stock)
 imports = open ('/home/bohdan/importfile/import.txt', 'w') 
-for count in range(1, 10):
+for count in range(0, int(count)):
 		partproductid1 = randint(1000, 9999)
 		partproductid2 = randint(1000, 9999)
 		partproductid3 = randint(1000, 9999)
@@ -29,7 +30,8 @@ for count in range(1, 10):
 		verkoopprijs = adviesprijs + 20		
  		imports.write("{}-{}-{}-N{}\t{}-{}\t{}\t{}\t{}\t{}\t{}\n".format(partproductid1, partproductid2, partproductid3, partproductid4, partproductid1, 
  										vendorsecondint, Namegenerator.name(), Namegenerator.username(), adviesprijs, verkoopprijs, choice(productstock))),
-print "File was created successfuly "
+print "File was created successfuly with",count+1,"products"
+
 imports.close()
 
 
