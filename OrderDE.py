df814c3e67b44483a83a4d8cd016d7abe493b985
Bohdan <radchenkobodya@gmail.com>
2015-12-28 15:38:28 +0200

    Create for the DE

---
+++ OrderDE.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+from startwork import start_webdriver
+from selenium.webdriver.common.by import By
+import unittest
+import random
+from  Button_from_fo import *
+import time
+
+
+
+class test_orderDE(unittest.TestCase, start_webdriver):
+
+    def setUp(self):
+
+            self.start_work_DE()
+            self.paymethodde = ['paypal', 'visa', 'mastercard', 'aexpress']
+            self.ran = random.randrange(2, 4)
+
+    def test_orderde(self):
+
+
+        for pmname in self.paymethodde:
+
+            for x in range(0, self.ran):
+
+
+                time.sleep(2)
+                self.send_keys_id(search, (random.choice(self.product)))
+                time.sleep(2)
+                self.click_by_xpath(add_to_card)
+
+            self.click_by_xpath(go_card)
+            time.sleep(2)
+            self.click_by_xpath(two_step_in_card)
+            time.sleep(2)
+            self.click_by_id(choose_default)
+            time.sleep(2)
+            self.click_by_xpath(go_pm_page)
+
+            time.sleep(2)
+            print (pmname)
+            self.click_by_id('{}'.format(pmname))
+            time.sleep(2)
+            self.click_by_xpath(buy_button)
+            time.sleep(2)
+            self.click_by_id('agree')
+            time.sleep(2)
+            self.click_by_xpath("//button[@class='btncta icon buy large checkout pull-right']")
+            time.sleep(2)
+            self.driver.get("http://futurumshop.de")
+
