14245c01ea35e3531017608792930e9a37aae6dc
Bohdan <radchenkobodya@gmail.com>
2015-12-28 12:55:11 +0200

    Done

--- testimport/createimport.py
+++ testimport/createimport.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from random import *
 from faker import Faker
 '''
@@ -9,21 +11,21 @@ for a little import's test.
 #make a product name
 Namegenerator = Faker()
 
-count = raw_input("How much products you need ?: ")
+#count = raw_input("How much products you need?: ")
 #product stock
 productstock = ['2-6 werkdagen', 'ja, op voorraad', 'Onbekende levertijd', 'week 1',
 				'week 10', 'week 11', 'week 12', 'week 13']
 
 #creating import file with 7 columns (vendorID, productID, vendorName,  productName, adviesprijs, verkoopprijs, product stock)
 imports = open ('/home/bohdan/importfile/import.txt', 'w') 
-for count in range(0, int(count)):
+for count in range(0, 7):
 		partproductid1 = randint(1000, 9999)
 		partproductid2 = randint(1000, 9999)
 		partproductid3 = randint(1000, 9999)
 		partproductid4 = randint(1000, 9999)
 		vendorsecondint = randint(1000, 9999)
 		adviesprijs = randint(10, 200)
-		verkoopprijs = adviesprijs + 20		
+		verkoopprijs = adviesprijs + 20
  		imports.write("{}-{}-{}-N{}\t{}-{}\t{}\t{}\t{}\t{}\t{}\n".format(
  							partproductid1, partproductid2, partproductid3, partproductid4, partproductid1, 
  							vendorsecondint, Namegenerator.name(), Namegenerator.username(), adviesprijs, verkoopprijs, choice(productstock))),
