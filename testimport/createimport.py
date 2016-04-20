09ebd080025f832d1547b7f539100cff1029a60e
BogdanRa <radchnekobodya@gmail.com>
2016-04-20 15:07:28 +0300

    Change prodlevID

--- testimport/createimport.py
+++ testimport/createimport.py
@@ -7,7 +7,7 @@ Creating import file with 7 important columns (vendorID, productID, vendorName, 
 for a little import's test. 
 '''
 
-#make massive with a vendors name	
+#make massive with vendor name	
 #make a product name
 Namegenerator = Faker()
 
@@ -18,15 +18,15 @@ productstock = ['2-6 werkdagen', 'ja, op voorraad', 'Onbekende levertijd', 'week
 
 #creating import file with 7 columns (vendorID, productID, vendorName,  productName, adviesprijs, verkoopprijs, product stock)
 imports = open ('/home/bohdan/importfile/import.txt', 'w') 
-for count in range(0, 7):
+for count in range(100):
 		partproductid1 = randint(1000, 9999)
 		partproductid2 = randint(1000, 9999)
-		partproductid3 = randint(1000, 9999)
+		partproductid3 = randint(1, 6)
 		partproductid4 = randint(1000, 9999)
 		vendorsecondint = randint(1000, 9999)
 		adviesprijs = randint(10, 200)
 		verkoopprijs = adviesprijs + 20
- 		imports.write("{}-{}-{}-N{}\t{}-{}\t{}\t{}\t{}\t{}\t{}\n".format(
+ 		imports.write("{}-{}-00{}-N{}\t{}-{}\t{}\t{}\t{}\t{}\t{}\n".format(
  							partproductid1, partproductid2, partproductid3, partproductid4, partproductid1, 
  							vendorsecondint, Namegenerator.name(), Namegenerator.company(), adviesprijs, verkoopprijs, choice(productstock))),
 
