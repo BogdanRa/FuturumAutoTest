b85444a537f133e6372c3662a15219d6608c4cdf
BogdanRa <radchnekobodya@gmail.com>
2016-05-13 12:09:47 +0300

    small refactoring

--- OrderDE.py
+++ OrderDE.py
@@ -9,7 +9,7 @@ class test_orderDE(unittest.TestCase, start_webdriver):
 
     def setUp(self):
         self.StartAndLogin(urlDE)
-        self.paymethodde = ['visa', 'mastercard', 'aexpress', 'sofort'] #'paypal'
+        self.paymethodde = ['visa', 'mastercard', 'aexpress']
         self.ran = random.randrange(2, 4)
 
     def test_orderde(self):
@@ -17,16 +17,9 @@ class test_orderDE(unittest.TestCase, start_webdriver):
         try:
             for pmname in self.paymethodde:
 
-                for x in range(0, self.ran):
-                    self.ranproduct = random.choice(self.product)
-                    print self.ranproduct
-                    self.send_keys_id(search, (self.ranproduct))
-                    self.click_by_xpath(add_to_card)
-
-                self.click_by_xpath(go_card)
-                self.click_by_xpath(two_step_in_card)
-                self.click_by_id(choose_default)
-                self.click_by_xpath(go_pm_page)
+                for totalproductincart in range(self.ran):
+                    self.generatecart()
+                self.verificationsteps()
 
 
                 self.click_by_id('{}'.format(pmname))
@@ -37,7 +30,7 @@ class test_orderDE(unittest.TestCase, start_webdriver):
                 self.driver.get(urlDE)
 
         except NoSuchElementException:
-            print ("last element not found", self.ranproduct)
+            print ("last element not found")
 
 
 
